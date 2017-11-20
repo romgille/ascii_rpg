7c5363299bf09b9868d220a58bc773f44934110d
Huy-Hai Vincent NGUYEN <nguyhuy@pc5301-test.esiee.fr>
2017-11-20 16:03:01 +0100

    add enum direction for individual and monster

---
+++ direction.hh
@@ -0,0 +1,6 @@
+enum Direction {
+	NORTH,
+	WEST,
+	SOUTH,
+	EAST	
+};
--- main.cc
+++ main.cc
@@ -2,8 +2,8 @@
 #include <iostream>
 
 int main() {
-  std::string data[] = {
-    #include "data.txt"
-  };
-  for (auto s : data) std::cout << s << std::endl;
+	std::string data[] = {
+		#include "data.txt"
+	};
+	for (auto s : data) std::cout << s << std::endl;
 }
