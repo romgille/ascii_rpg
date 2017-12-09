5b10a3fa410834061e4a90edae2ff0b510c43b24
NGUYEN Huy-Hai Vincent <nhhvincent@gmail.com>
2017-12-09 20:24:25 +0100

    comments

--- Position.hh
+++ Position.hh
@@ -2,24 +2,24 @@
 #define POSITION_HH
 
 class Position {
-private:
-  int x;
-  int y;
+  private:
+    int x;
+    int y;
 
-public:
-  Position();
-  Position(int x_, int y_);
-  ~Position() = default;
+  public:
+    Position();
+    Position(int x_, int y_);
+    ~Position() = default;
 
-  int getX();
-  void setX(int x_);
+    int getX();
+    void setX(int x_);
 
-  int getY();
-  void setY(int y_);
+    int getY();
+    void setY(int y_);
 
-  bool operator==(Position p);
+    bool operator==(Position p);
 
-  Position operator+(Position p);
+    Position operator+(Position p);
 };
 
-#endif
+#endif /* end of include guard: POSITION_HH */
